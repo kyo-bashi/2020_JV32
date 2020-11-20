b80942b586d6ddae96cc20f17dcf481e52b6270b
kyobashi <1998ryuo1023gmail.co>
2020-11-20 10:20:12 +0900

    first

---
+++ WEB-INF/src/myServlet/R01_Servlet.java
@@ -0,0 +1,44 @@
+package myServlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+//HttpServletインポート
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"r01_servlet"})
+public class R01_Servlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+
+			//supper.doGet(req,resp);
+			//↑supper.doGetは実行すると必ずエラーになる
+			fncServlet(req,resp);
+		}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+
+			//supper.doGet(req,resp);
+			//↑supper.doGetは実行すると必ずエラーになる
+			fncServlet(req,resp);
+		}
+
+
+
+	protected void fncServlet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException{
+		//	サーブレットの処理を書く
+		req.setCharacterEncoding("UTF8");
+
+		// デバッグ用出力例
+		System.out.println("step-01");
+	}
+
+}
---
+++ jsp/R01_input.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<%
+
+	//パスの取得
+	String strPath = request.getCotextpath();;
+
+%>
+<body>
+<h4>R01_input.jsp</h4><br />
+<form action="R01_output.jsp" method="get">
+  <input type="text" name="txtName" /><br />
+  <input type="submit" value="送信" />
+</form>
+</body>
+</html>
